2c869864c08de4998d51983d9b4dd9f616fa46dd
DarkLordDev <microansh2@gmail.com>
2022-06-16 17:38:35 +0530

    upd 0.0.1

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const HomePage = () => {
+	return (
+		<>
+			<Head>
+				<title>iShop | Home</title>
+			</Head>
+			<h1 className="text-3xl text-center font-bold">Hello world!</h1>
+		</>
+	);
+};
+
+export default HomePage;
